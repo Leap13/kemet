f47093ef018073021f01ddfa4174e1dbc92de44e
aaharidy <abdoharidy72@gmail.com>
2022-03-08 15:23:17 +0200

    full width style

--- inc/blocks-styles.php
+++ inc/blocks-styles.php
@@ -15,10 +15,21 @@ function register_block_styles() {
 			'name'         => 'fit-to-screen',
 			'label'        => __( 'Fit to Screen', 'kemet' ),
 			'inline_style' => '
-			.is-style-fit-to-screen {
+			.is-style-fit-to-screen > * {
+				max-width: 100vw;
+			}',
+		),
+	);
+	register_block_style( // phpcs:ignore WPThemeReview.PluginTerritory.ForbiddenFunctions.editor_blocks_register_block_style
+		'core/group',
+		array(
+			'name'         => 'full-height',
+			'label'        => __( 'Full Height', 'kemet' ),
+			'inline_style' => '
+			.is-style-full-height {
 				height: 100vh;
 			}',
-		)
+		),
 	);
 }
 add_action( 'init', 'register_block_styles' );
